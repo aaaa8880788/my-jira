efc11eef0dc8446ec3b34c619d42a8e009a098c6
zhengjianfeng <zhengjianfeng@163.com>
2023-10-13 16:49:01 +0800

    code submit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx'
 import { AppProviders } from './context/index'
 import './assets/css/global.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
